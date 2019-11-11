b0f422db462e2a566a669081cc5e38568fcab79c
Kumar gaurav mishra <kumar@Kumars-MacBook-Pro.local>
2019-11-11 09:00:10 +0530

    added add vertex and add edge methods

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 'use strict';
 class Graph {
-  constructor() {}
+  constructor() {
+    this.adjacencyList = [];
+  }
+  addVertex(vertex) {
+    if (!vertex) return undefined;
+    if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = [];
+    return this;
+  }
+  addEdge(vertex1, vertex2) {
+    if (!vertex1 || !vertex2) return undefined;
+    if (this.adjacencyList[vertex1]) {
+      this.adjacencyList[vertex1].push(vertex2);
+    } else {
+      this.adjacencyList[vertex1] = [vertex2];
+    }
+    if (this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex2].push(vertex1);
+    } else {
+      this.adjacencyList[vertex2] = [vertex1];
+    }
+  }
 }
 module.exports = Graph;
