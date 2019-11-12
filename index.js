8effdadece86acc046c4d8ecb960a1bb6e418b87
Kumar gaurav mishra <kumar@Kumars-MacBook-Pro.local>
2019-11-12 09:03:10 +0530

    Added add vertex, edge, remove edge and vertex methods

--- index.js
+++ index.js
@@ -20,6 +20,29 @@ class Graph {
     } else {
       this.adjacencyList[vertex2] = [vertex1];
     }
+    return this;
+  }
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1]) this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2);
+    if (this.adjacencyList[vertex2]) this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1);
+    return this;
+  }
+  removeVertex(vertex) {
+    let arr = this.adjacencyList[vertex];
+    if (arr && arr.length >= 0) {
+      delete this.adjacencyList[vertex];
+      arr.forEach(val => {
+        this.adjacencyList[val] = this.adjacencyList[val].filter(v => v !== vertex);
+      });
+    }
+    return this;
   }
 }
+let graph = new Graph();
+['kumar', 'jyoti', 'avichal'].forEach(val => graph.addVertex(val));
+graph.addEdge('kumar', 'avichal');
+graph.addEdge('kumar', 'jyoti');
+graph.addEdge('jyoti', 'avichal');
+graph.removeEdge('kumar', 'jyoti');
+console.log(graph.adjacencyList);
 module.exports = Graph;
